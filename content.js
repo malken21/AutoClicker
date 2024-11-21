30a38b76b2ff824b5d4b825e5256c6e5125abaf6
Marumasa <77374813+malken21@users.noreply.github.com>
2024-11-21 22:16:25 +0900

    Update content.js

--- content.js
+++ content.js
@@ -1,16 +1,18 @@
-const selector = 'dev#test'; // ここにクリックしたい要素のセレクタを記入
+const selectors = ['dev#test']; // ここにクリックしたい要素のセレクタを記入
 const wait = 5000; // クリックするまでの待機時間 単位はミリ秒
 
 // ページが読み込まれたら実行
 window.addEventListener('load', () => {
-    const elements = document.querySelectorAll(selector);
-    elements.forEach(async (element) => {
-        if (element) {
-            await sleep(5000); // 待機
-            element.click(); // 要素をクリック
-            console.log(`${selector} click !`);
-        }
-    })
+    selectors.forEach((selector) => {
+        const elements = document.querySelectorAll(selector);
+        elements.forEach(async (element) => {
+            if (element) {
+                await sleep(5000); // 待機
+                element.click(); // 要素をクリック
+                console.log(`${selector} click !`);
+            }
+        });
+    });
 });
 
 function sleep(ms) {
